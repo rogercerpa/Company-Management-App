3e27f209601b67da4bdff06ea51b4f138b1a1988
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-02-28 18:49:27 -0500

    added basic departments and roles to the database

--- seeds.sql
+++ seeds.sql
@@ -1,1 +1,18 @@
 USE company_management;
+
+INSERT INTO departments
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Manager', 50000, 1),
+    ('Account Manager', 60000, 1),
+    ('Application Engineer', 70000, 3),
+    ('Application Engineer Manager', 80000, 1),
+    ('Lead Application Engineer', 75000, 2),
+    ('Account assistance', 40000, 2);
