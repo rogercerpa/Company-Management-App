b0a3e519edfabaa80f7c11d3f521db09ed78e6d4
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-02-29 14:22:40 -0500

    added initial employees to the database

--- seeds.sql
+++ seeds.sql
@@ -16,3 +16,10 @@ VALUES
     ('Application Engineer Manager', 80000, 1),
     ('Lead Application Engineer', 75000, 2),
     ('Account assistance', 40000, 2);
+
+INSERT INTO employee
+    (first_name, last_name, role_id)
+VALUES
+    ('Roger', 'Cerpa', 1),
+    ('Eduardo', 'DeCastro', 2),
+    ('Jesus', 'Barrios', 3)
