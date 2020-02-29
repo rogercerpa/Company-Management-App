65d34643982a88fac91ab907169bffe4b94468c9
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-02-29 14:22:05 -0500

    updated the database to include more values

--- schema.sql
+++ schema.sql
@@ -5,27 +5,44 @@ use company_management;
 
 CREATE TABLE departments
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT NOT NULL
+    AUTO_INCREMENT,
+    name VARCHAR
+    (30) NOT NULL,
+    
+    PRIMARY KEY
+    (id)
 );
 
-CREATE TABLE role
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL (12,4) NOT NULL,
+    CREATE TABLE role
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT,
+    title VARCHAR
+        (30) NOT NULL,
+    salary DECIMAL
+        (12,4) NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY
+        (id)
+    FOREIGN KEY
+        (department_id) REFERENCES departments
+        (id)
 );
 
-CREATE TABLE employee
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+        CREATE TABLE employee
+        (
+            id INT NOT NULL
+            AUTO_INCREMENT,
+    first_name VARCHAR
+            (30) NOT NULL,
+    last_name VARCHAR
+            (30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY
+            (id)
+    FOREIGN KEY
+            (role_id) REFERENCES role
+            (id)
 );
-
